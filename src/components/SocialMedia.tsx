48056823f5a6335f379ec55dd9d86e83607a1d28
israel Ogunsua <Oluwasunisrael1990@gmail.com>
2024-08-09 11:58:04 -0600

    made app better  responsive

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -1,88 +1,71 @@
 import React from "react";
-import { Box, IconButton } from "@mui/material";
+import { Box, IconButton, IconButtonProps } from "@mui/material";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import YouTubeIcon from "@mui/icons-material/YouTube";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import InstagramIcon from "@mui/icons-material/Instagram";
-import SchoolIcon from '@mui/icons-material/School';
-import LanguageIcon from "@mui/icons-material/Language";
-import HomeIcon from '@mui/icons-material/Home';
+import SchoolIcon from "@mui/icons-material/School";
+import HomeIcon from "@mui/icons-material/Home";
+
 const SocialMediaLinks: React.FC = () => {
   return (
     <Box sx={{ display: "flex", gap: "0.5rem" }}>
-      <IconButton
-        component="a"
-        href="https://www.linkedin.com/company/everything-ai-coding-association/"
-        target="_blank"
-        rel="noopener noreferrer"
-        color="default"
-      >
-        <LinkedInIcon />
-      </IconButton>
-      <IconButton
-        component="a"
-        href="https://www.youtube.com/channel/UCe_UvdY56qF7Q3B9AKfzQsQ"
-        target="_blank"
-        rel="noopener noreferrer"
-         color="default"
-      >
-        <YouTubeIcon />
-      </IconButton>
-      <IconButton
-        component="a"
-        href="https://www.facebook.com/profile.php?id=61562120125882"
-        target="_blank"
-        rel="noopener noreferrer"
-         color="default"
-      >
-        <FacebookIcon />
-      </IconButton>
-      <IconButton
-        component="a"
-        href="https://x.com/EverthingAICode"
-        target="_blank"
-        rel="noopener noreferrer"
-         color="default"
-      >
-        <TwitterIcon />
-      </IconButton>
-      <IconButton
-        component="a"
-        href="https://www.instagram.com/_everything_ai/?hl=en"
-        target="_blank"
-        rel="noopener noreferrer"
-         color="default"
-      >
-        <InstagramIcon />
-      </IconButton>
-      <IconButton
-        component="a"
-        href="https://www.youtube.com/channel/UCe_UvdY56qF7Q3B9AKfzQsQ"
-        target="_blank"
-        rel="noopener noreferrer"
-        color="default"
-      >
-       <YouTubeIcon/>
-      </IconButton>
-      <IconButton
-        component="a"
-        href="https://campus.everythingai.ca/"
-        target="_blank"
-        rel="noopener noreferrer"
-        color="warning"
-      >
-        <SchoolIcon/>
-      </IconButton>
-      <IconButton
-        component="a"
-        href="https://everythingai.ca/"
-        target="_blank"
-        rel="noopener noreferrer"
-        color="success"
-      >
-        <HomeIcon />
-      </IconButton>
+      {[
+        {
+          href: "https://www.linkedin.com/company/everything-ai-coding-association/",
+          icon: <LinkedInIcon />,
+          color: "default" as IconButtonProps['color'],
+        },
+        {
+          href: "https://www.youtube.com/channel/UCe_UvdY56qF7Q3B9AKfzQsQ",
+          icon: <YouTubeIcon />,
+          color: "default" as IconButtonProps['color'],
+        },
+        {
+          href: "https://www.facebook.com/profile.php?id=61562120125882",
+          icon: <FacebookIcon />,
+          color: "default" as IconButtonProps['color'],
+        },
+        {
+          href: "https://x.com/EverthingAICode",
+          icon: <TwitterIcon />,
+          color: "default" as IconButtonProps['color'],
+        },
+        {
+          href: "https://www.instagram.com/_everything_ai/?hl=en",
+          icon: <InstagramIcon />,
+          color: "default" as IconButtonProps['color'],
+        },
+        {
+          href: "https://campus.everythingai.ca/",
+          icon: <SchoolIcon />,
+          color: "warning" as IconButtonProps['color'],
+        },
+        {
+          href: "https://everythingai.ca/",
+          icon: <HomeIcon />,
+          color: "success" as IconButtonProps['color'],
+        },
+      ].map((item, index) => (
+        <IconButton
+          key={index}
+          component="a"
+          href={item.href}
+          target="_blank"
+          rel="noopener noreferrer"
+          color={item.color}
+          sx={{
+            transition: "background-color 0.3s ease",
+            "&:hover": {
+              backgroundColor: "white",
+              borderRadius: "50%",
+            },
+          }}
+        >
+          {item.icon}
+        </IconButton>
+      ))}
     </Box>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Container, Typography, Grid } from "@mui/material";
+import { Box, Container, Typography, Grid, useMediaQuery, useTheme } from "@mui/material";
 import MetaData from "../components/Metadata";
 import MainPageBox from "../components/PageBox";
 import Sponsor from "../components/ourSponsor"; 
@@ -7,70 +7,79 @@ import OurWork from "../components/OurWork";
 import BoxSection from "../components/BoxSection";
 import ImgMediaCard from "../components/Cards";
 import TestimonialSection from "../components/Testimonial";
-import image1 from "../assets/program1.jpg"
-import image2 from "../assets/program3.jpg"
-import image3 from "../assets/kids2.jpg"
+import image1 from "../assets/program1.jpg";
+import image2 from "../assets/program3.jpg";
+import image3 from "../assets/program2.jpg";
 
 const Homepage: React.FC = () => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
   return (
     <div>
       <MetaData
         title="Home | EverythingAI-SandBox"
         description="Welcome to EverythingAI Home"
       />
-
+         
+      <Box sx={{ py: { xs: 2, md: 4 } }}>
       <MainPageBox />
-
-      <Box sx={{ py: 4 }}>
-        <Container maxWidth="lg" fixed>
+        <Container maxWidth="lg">
+       
           <Sponsor />
           <OurWork />
           <BoxSection />
 
-          <Grid container spacing={3} justifyContent="center" sx={{ mb: 5 }}>
-            <Grid item xs={12}>
-              <Typography
-                variant="h5"
-                gutterBottom
-                sx={{
-                  fontWeight: "bold",
-                  color: "#0097B2",
-                  textAlign: "center",
-                  mb: 5
-                }}
-              >
-                Our Programs and Activities
-              </Typography>
-            </Grid>
-            
-            <Grid container item spacing={3} justifyContent="center">
-              <Grid item xs={12} sm={4}>
-              <ImgMediaCard
-                url="program"
-                title="Beginners"
-                description="Ages 7-10 Teach kids to code in simple programs Give kids a basic understanding of code Kids learn Scratch and Python."
-                image={image1}
-              />
+          <Grid
+              container
+              spacing={3}
+              justifyContent="center"
+              alignItems="center"
+              sx={{ mb: 5, justifyContent:"center", alignItems:"center"}}
+            >
+              <Grid item xs={12}>
+                <Typography
+                  variant={isMobile ? "h6" : "h5"}
+                  gutterBottom
+                  sx={{
+                    fontWeight: "bold",
+                    color: "#0097B2",
+                    textAlign: "center",
+                    mb: { xs: 3, md: 5 },
+                  }}
+                >
+                  Our Programs and Activities
+                </Typography>
+              </Grid>
 
-              </Grid>
-              <Grid item xs={12} sm={4}>
-              <ImgMediaCard
-                url="program"
-                title="Intermidiate Program"
-                description="Ages 11 – 14 Introduction to Programs such as Python & Scratch Provides understanding of coding to kids"
-                image={image2}
-              />
-              </Grid>
-              <Grid item xs={12} sm={4}>
-              <ImgMediaCard
-                url="/program"
-                title="Elite Program"
-                description="Ages 15+ Web Development App Development Interactive Game Development Security Programs."
-                image={image3}
-              />
+              <Grid container item spacing={3} justifyContent="center">
+                <Grid item xs={12} sm={6} md={4} sx={{ textAlign: 'center' }}>
+                  <ImgMediaCard
+                    url="program"
+                    title="Beginners"
+                    description="Ages 7-10 Teach kids to code in simple programs Give kids a basic understanding of code Kids learn Scratch and Python."
+                    image={image3}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6} md={4} sx={{ textAlign: 'center' }}>
+                  <ImgMediaCard
+                    url="program"
+                    title="Intermediate Program"
+                    description="Ages 11 – 14 Introduction to Programs such as Python & Scratch Provides understanding of coding to kids"
+                    image={image2}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6} md={4} sx={{ textAlign: 'center' }}>
+                  <ImgMediaCard
+                    url="/program"
+                    title="Elite Program"
+                    description="Ages 15+ Web Development App Development Interactive Game Development Security Programs."
+                    image={image1}
+                  />
+                </Grid>
               </Grid>
             </Grid>
-          </Grid>
+
 
           <TestimonialSection />
         </Container>
